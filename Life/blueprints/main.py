0e15ea74d211b75213d9286ad7d74759287f9068
zchen <446069867@qq.com>
2020-01-13 21:58:53 +0800

    four

--- Life/blueprints/main.py
+++ Life/blueprints/main.py
@@ -3,7 +3,7 @@ from flask_login import current_user, login_required
 from werkzeug.utils import redirect
 
 from Life.form.main import PostForm, CommentForm
-from Life.models import db, Post, Comment
+from Life.models import db, Post, Comment, User
 
 main_bp = Blueprint('main',__name__)
 
@@ -32,3 +32,48 @@ def post(id):
         return redirect(url_for('main.post',id=post.id))
     comments = Comment.query.filter_by(post_id=id).order_by(Comment.timestamp.desc()).all()
     return render_template('main/post.html',post=post,form=form,comments=comments)
+
+@main_bp.route('/post/<int:id>/edit',methods=['GET','POST'])
+@login_required
+def edit(id):
+    post = Post.query.get_or_404(id)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.body = form.body.data
+        db.session.add(post)
+        db.session.commit()
+        return  redirect(url_for('main.post',id=post.id))
+    form.body.data = post.body
+    return render_template('main/edit_post.html',form=form)
+
+@main_bp.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('main/user.html',user=user,posts=posts)
+
+
+@main_bp.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if current_user.is_following(user):
+        flash('你已经关注过了','err')
+        return redirect(url_for('main.user',username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash('关注成功','ok')
+    return redirect(url_for('main.user',username=username))
+
+@main_bp.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if not current_user.is_following(user):
+        flash('未关注此用户','err')
+        return redirect(url_for('main.user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash('已取消关注','ok')
+    return redirect(url_for('main.user', username=username))
+
--- Life/models.py
+++ Life/models.py
@@ -6,6 +6,11 @@ from werkzeug.security import check_password_hash
 
 from . import db,login_manager
 
+class Follow(db.Model):
+    __tablename__ = 'follows'
+    follower_id = db.Column(db.Integer,db.ForeignKey('users.id'),primary_key=True)
+    followed_id = db.Column(db.Integer,db.ForeignKey('users.id'),primary_key=True)
+    timestamp = db.Column(db.DateTime,default=datetime.utcnow)
 
 class User(db.Model,UserMixin):
     __tablename__ = 'users'
@@ -14,13 +19,20 @@ class User(db.Model,UserMixin):
     email = db.Column(db.String(254),unique=True,index=True)
     password_hash = db.Column(db.String(128))
     website = db.Column(db.String(255),default=None)
-    bio = db.Column(db.String(120),default=None)
     location = db.Column(db.String(50),default=None)
+    about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime,default=datetime.now())
     confirmed = db.Column(db.Boolean,default=False)
+    last_seen = db.Column(db.DateTime(),default=datetime.utcnow)
     posts = db.relationship('Post',backref='author',lazy='dynamic')
     active = db.Column(db.Boolean,default=True)
     comments = db.relationship('Comment',backref='author',lazy='dynamic')
+    followed = db.relationship('Follow',foreign_keys=[Follow.follower_id],
+                               backref=db.backref('follower',lazy='joined'),lazy='dynamic',
+                               cascade='all,delete-orphan')
+    followers = db.relationship('Follow',foreign_keys=[Follow.followed_id],
+                                backref=db.backref('followed',lazy='joined'),lazy='dynamic',
+                                cascade='all,delete-orphan')
 
     def register(user):
         try:
@@ -37,6 +49,21 @@ class User(db.Model,UserMixin):
     def is_active(self):
         return self.active
 
+    def follow(self,user):
+        if not self.is_following(user):
+            f = Follow(follower=self,followed=user)
+            db.session.add(f)
+
+    def unfollow(self,user):
+        f = self.followed.filter_by(followed_id=user.id).first()
+        if f:
+            db.session.delete(f)
+
+    def is_following(self,user):
+        return self.followed.filter_by(followed_id=user.id).first() is not None
+
+    def is_followed_by(self,user):
+        return self.followers.filter_by(follower_id=user.id).first() is not None
 
 class Post(db.Model):
     __tablename__ = 'posts'
